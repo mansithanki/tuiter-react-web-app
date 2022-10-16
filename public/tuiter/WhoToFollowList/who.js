39d1386dd97a99b13f80391d14cad904e22a456b
mansithanki <thankimansi22@gmail.com>
2022-10-16 19:33:43 -0400

    a5 JavaScript sp22

--- public/tuiter/WhoToFollowList/who.js
+++ public/tuiter/WhoToFollowList/who.js
@@ -1,5 +1,5 @@
 export default [
-    {   avatarIcon: '../images/JAVA.png',
+    {   avatarIcon: '../images/javablack.png',
       userName: 'Java', handle: 'Java', },
     {   avatarIcon: '../images/RELATIVITY.png',
       userName: 'Relativity Space',
@@ -7,9 +7,9 @@ export default [
     {   avatarIcon: '../images/virgin.png',
       userName: 'Virgin Galactic',
       handle: 'virgingalactic', },
-    {   avatarIcon: '../images/NASA.png',
+    {   avatarIcon: '../images/nasablack.png',
       userName: 'NASA', handle: 'NASA', },
-    {   avatarIcon: '../images/tesla.png',
+    {   avatarIcon: '../images/teslablack.png',
       userName: 'Tesla', handle: 'Tesla', }, ];
    
      
