dcb618084dc187540cfc454c6485aa9d7b68155c
mansithanki <thankimansi22@gmail.com>
2022-11-05 19:48:37 -0400

    a7 Redux fa22

--- src/nav.js
+++ src/nav.js
@@ -4,10 +4,27 @@ function Nav() {
  return (
    <div>
      <Link to="/">Labs</Link> |
-     <Link to="/hello">Hello</Link> |
-     <Link to="/tuiter/home">Tuiter</Link>
+     <Link to="/">Assignment 6</Link> |
+     <Link to="/a7">Assignment 7</Link> |
+     <Link to="/tuiter">Tuiter</Link>
    </div>
- )
+ );
 }
 
 export default Nav;
+
+
+
+// import {Link} from "react-router-dom";
+
+// function Nav() {
+//  return (
+//    <div>
+//      <Link to="/">Labs</Link> |
+//      <Link to="/hello">Hello</Link> |
+//      <Link to="/tuiter/home">Tuiter</Link>
+//    </div>
+//  )
+// }
+
+// export default Nav;
